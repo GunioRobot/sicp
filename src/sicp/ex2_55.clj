6e9b570e1d54c1c3d89ba42b4c60d618c5d5a36b
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2010-09-04 09:08:11 +0530

    Added a simple explanation.

--- src/sicp/ex2_55.clj
+++ src/sicp/ex2_55.clj
@@ -4,3 +4,9 @@
 (deftest test-double-quote
   (is [= (first ''abracadabra)
          'quote]))
+
+(comment
+  "(first ''abcde) expands to (first (quote (quote (abcde)))
+  which is (quote (abcde)), first of which is quote. The outer
+  quote makes the inner quote a symbol and does not evaluate it." 
+  )
