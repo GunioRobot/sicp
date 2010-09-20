8905f914a0a2174f4984564dd857d49ada87cda4
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2010-09-20 22:20:40 +0530

    code in the messaging passing section

--- src/sicp/ch2_4.rkt
+++ src/sicp/ch2_4.rkt
@@ -103,4 +103,16 @@
 (define (make-from-mag-ang r a)
   (make-from-mag-ang-polar r a))
 
-;;; data directed programming
+;;; message passing
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond
+      [(eq? op 'real-part) x]
+      [(eq? op 'imag-part) y]
+      [(eq? op 'magnitude)
+       (sqrt (+ (square x) (square y)))]
+      [(eq? op 'angle) (atan y x)]
+      [else 
+       (error "unknown op" op)]))
+  dispatch)
+
