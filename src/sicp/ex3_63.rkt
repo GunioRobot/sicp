258e106b52a032afd2464f41ac92e5d20487d7f8
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2011-07-23 20:19:31 +0530

    solution to 3.63. A little tricky to describe

---
+++ src/sicp/ex3_63.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+#|
+
+memo-proc cannot handle recursive definitions. With the way it is defined in the question, 
+we have a recursive 'call' to sqrt-stream and hence produced another entirely different 
+stream whereas in the original definition, 'guesses' gets forced. 
+
+|#
